e0e3c64292658853a67b43a72ef4b7daceff05f7
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-04-11 21:37:57 -0300

    Dotnet cli

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aspcore_001_sofn_primeiro_projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
