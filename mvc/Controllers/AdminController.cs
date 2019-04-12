152bddac562b236bf8370b107f092fdf18c18817
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-04-11 23:11:30 -0300

    Rotas

--- mvc/Controllers/AdminController.cs
+++ mvc/Controllers/AdminController.cs
@@ -3,13 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace mvc.Controllers
 {
+    [Route("painel/admin")]
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        [HttpGet("principal/{nome}/{numero:int?}")]
+        public IActionResult Index(string nome, int numero)
         {
-            return Content("Hello World!");
+            return Content("Vc "+nome+" é nota " + numero);
         }
 
+        [HttpGet("rota1")]
+        [HttpGet("rota2")]
         public IActionResult Teste()
         {
             return Content("Aprendendo ASP.NET Core");
